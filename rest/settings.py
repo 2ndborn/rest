92f23611e8aac9dc0701b84bbfebf3b278eb7a6e
Micah Francis <micah.francis69@googlemail.com>
2024-08-29 19:41:51 +0000

    updated settings.py

--- rest/settings.py
+++ rest/settings.py
@@ -51,7 +51,7 @@ JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
 JWT_AUTH_SAMESITE = 'None'
 
 REST_AUTH_SERIALIZERS = {
-    'USER_DETAILS_SERIALIZER': 'drf_api.serializers.CurrentUserSerializer'
+    'USER_DETAILS_SERIALIZER': 'rest.serializers.CurrentUserSerializer'
 }
 
 # Quick-start development settings - unsuitable for production
@@ -96,15 +96,6 @@ INSTALLED_APPS = [
 ]
 
 SITE_ID = 1
-
-REST_USE_JWT = True
-JWT_AUTH_COOKIE = 'my-app-auth'
-JWT_AUTH_SECURE = True
-JWT_AUTH_REFRESH_COOKIE = 'my-refresh-token'
-
-REST_AUTH_SERIALIZERS = {
-    'USER_DETAILS_SERIALIZER': 'rest.serializers.CurrentUserSerializer'
-}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
